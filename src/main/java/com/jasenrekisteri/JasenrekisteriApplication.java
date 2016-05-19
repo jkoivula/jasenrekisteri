8f791de8c385e60da0463e3eb30c63bbb0bd473c
Joni Koivula <joni.koivula@student.hamk.fi>
2016-05-19 12:24:01 +0300

    Projekti 'jasenrekisteri' aloitettu, spring initializr:lla.

---
+++ src/main/java/com/jasenrekisteri/JasenrekisteriApplication.java
@@ -0,0 +1,12 @@
+package com.jasenrekisteri;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JasenrekisteriApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JasenrekisteriApplication.class, args);
+	}
+}
